0819c8f30215c686f18c09ed2bd9933d3d47ce6d
Chelsea Hamilton <chels@Chelseas-iMac.local>
2023-02-11 20:50:46 -0600

    monkeymood

--- monkeyCalculator.py
+++ monkeyCalculator.py
@@ -1,11 +1,14 @@
 def calculateTime():
-    
-    # This first line is provided for you
-    monkey_one = input("Is the first monkey smiling?:  ")
+    monkey_one = input("Is the first monkey smiling?: ")
     monkey_two = input("Is the second monkey smiling?: ")
 
-    # end assignment
-
+    if monkey_one.lower() == "y" and monkey_two.lower() == "n":
+        print("Yay! We're going to have a good day!")
+    
+    elif monkey_one.lower() =="n" and monkey_two.lower() =="y":
+        print("Yay! We're going to have a good day!")
+    else:
+        print("Uh Oh! We're in trouble!")
 
 ## If you want to test locally run > python monkeyCalculator.py
 
